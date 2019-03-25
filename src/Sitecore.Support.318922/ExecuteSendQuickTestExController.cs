7eb9f61c27fce9aae8d7ff021414cb11a22b70af
Ivan Sheyenko <ivs@sitecore.net>
2019-03-25 17:06:50 -0400

    original code

---
+++ src/Sitecore.Support.318922/ExecuteSendQuickTestExController.cs
@@ -0,0 +1,206 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.Model.Web;
+using Sitecore.EmailCampaign.Model.Web.Exceptions;
+using Sitecore.EmailCampaign.Server.Contexts;
+using Sitecore.EmailCampaign.Server.Responses;
+using Sitecore.Modules.EmailCampaign.Core;
+using Sitecore.Modules.EmailCampaign.Messages;
+using System;
+using System.Web.Http;
+using Sitecore.Support.EmailCampaign.Server.Filters;
+using Sitecore.Services.Core;
+using Sitecore.Services.Infrastructure.Web.Http;
+using Sitecore.Modules.EmailCampaign.Validators;
+using Sitecore.Modules.EmailCampaign.Services;
+using Sitecore.EmailCampaign.Model.Web.Settings;
+using Sitecore.ExM.Framework.Diagnostics;
+using Sitecore.EmailCampaign.Server.Helpers;
+using Sitecore.EmailCampaign.Cm.Factories;
+using Sitecore.Configuration;
+using Sitecore.EmailCampaign.Model.Message;
+using Sitecore.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Sitecore.Data.Items;
+using Sitecore.Data;
+
+namespace Sitecore.Support.EmailCampaign.Server.Controllers.SendQuickTest
+{
+  [ServicesController("EXM.ExecuteSendQuickTest")]
+  [SitecoreAuthorize(new string[]
+{
+  "sitecore\\EXM Advanced Users",
+  "sitecore\\EXM Users"
+})]
+  public class ExecuteSendQuickTestExController: ServicesApiController
+  {
+    private readonly RegexValidator _emailRegexValidator;
+
+    private readonly IExmCampaignService _exmCampaignService;
+
+    private readonly GlobalSettings _globalSettings;
+
+    private readonly ILogger _logger;
+
+    private readonly RegistryHelper _registryHelper;
+
+    private readonly IAbnTestService _abnTestService;
+
+    private readonly ISendingManagerFactory _sendingManagerFactory;
+
+    public ExecuteSendQuickTestExController(IExmCampaignService exmCampaignService, ILogger logger, IAbnTestService abnTestService, ISendingManagerFactory sendingManagerFactory)
+      : this(exmCampaignService, GlobalSettings.Instance, new RegistryHelper(), logger, (RegexValidator)Factory.CreateObject("emailRegexValidator", true), abnTestService, sendingManagerFactory)
+    {
+    }
+
+    internal ExecuteSendQuickTestExController(IExmCampaignService exmCampaignService, GlobalSettings globalSettings, RegistryHelper registryHelper, ILogger logger, RegexValidator emailRegexValidator, IAbnTestService abnTestService, ISendingManagerFactory sendingManagerFactory)
+    {
+      Assert.ArgumentNotNull(exmCampaignService, "exmCampaignService");
+      Assert.ArgumentNotNull(globalSettings, "globalSettings");
+      Assert.ArgumentNotNull(registryHelper, "registryHelper");
+      Assert.ArgumentNotNull(logger, "logger");
+      Assert.ArgumentNotNull(emailRegexValidator, "emailRegexValidator");
+      Assert.ArgumentNotNull(abnTestService, "abnTestService");
+      Assert.ArgumentNotNull(sendingManagerFactory, "sendingManagerFactory");
+      _exmCampaignService = exmCampaignService;
+      _globalSettings = globalSettings;
+      _registryHelper = registryHelper;
+      _logger = logger;
+      _emailRegexValidator = emailRegexValidator;
+      _abnTestService = abnTestService;
+      _sendingManagerFactory = sendingManagerFactory;
+    }
+
+    [ActionName("DefaultAction")]
+    public Response ExecuteSendQuickTest(SendQuickTestContext data)
+    {
+      Assert.ArgumentNotNull(data, "requestArgs");
+      Assert.IsNotNull(data.TestEmails, "TestEmails must be supplied");
+      try
+      {
+        MessageItem messageItem = _exmCampaignService.GetMessageItem(Guid.Parse(data.MessageId), data.Language);
+        if (messageItem != null)
+        {
+          if (!_globalSettings.GetNoSend())
+          {
+            if (messageItem.State != MessageState.Sent)
+            {
+              string text = CheckAddressValidation(data.TestEmails);
+              if (string.IsNullOrEmpty(text))
+              {
+                AbnTest abnTest = _abnTestService.GetAbnTest(messageItem);
+                if (abnTest != null && abnTest.TestCandidates.Count > 1 && data.VariantIds.Length == 0)
+                {
+                  return new Response
+                  {
+                    ErrorMessage = EcmTexts.Localize("The quick test cannot be sent. Select one or more variants and try again.", Array.Empty<object>()),
+                    Error = true
+                  };
+                }
+                _registryHelper.LastTestEmail = data.TestEmails;
+                try
+                {
+                  RunQuickTest(data.TestEmails, data.VariantIds, messageItem, abnTest);
+                }
+                catch (EmailCampaignException ex)
+                {
+                  return new Response
+                  {
+                    ErrorMessage = ex.LocalizedMessage,
+                    Error = true
+                  };
+                }
+                return new StringResponse
+                {
+                  Value = EcmTexts.Localize("The message has been sent.", Array.Empty<object>())
+                };
+              }
+              return new Response
+              {
+                ErrorMessage = text,
+                Error = true
+              };
+            }
+            return new Response
+            {
+              ErrorMessage = EcmTexts.Localize("Unable to send the quick test as the message has been sent.", Array.Empty<object>()),
+              Error = true
+            };
+          }
+          return new Response
+          {
+            ErrorMessage = EcmTexts.Localize("Message dispatch is disabled by the system. Change this in the Global Settings menu in EXM or contact your administrator.", Array.Empty<object>()),
+            Error = true
+          };
+        }
+        return new Response
+        {
+          ErrorMessage = EcmTexts.Localize("Edited message could not be found. It may have been moved or deleted by another user.", Array.Empty<object>()),
+          Error = true
+        };
+      }
+      catch (Exception ex2)
+      {
+        _logger.LogError(ex2.Message, ex2);
+        return new Response
+        {
+          ErrorMessage = EcmTexts.Localize("A serious error occurred please contact the administrator", Array.Empty<object>()),
+          Error = true
+        };
+      }
+    }
+
+    private string CheckAddressValidation(string emailAddresses)
+    {
+      ListString listString = new ListString(emailAddresses, ',');
+      if (listString.Count == 0)
+      {
+        return EcmTexts.Localize("'{0}' is not a valid email address. To send the quick test, please enter a valid email address and try again.", string.Empty);
+      }
+      using (IEnumerator<string> enumerator = (from email in listString
+                                               where !_emailRegexValidator.IsValid(email.Trim())
+                                               select email).GetEnumerator())
+      {
+        if (enumerator.MoveNext())
+        {
+          string current = enumerator.Current;
+          return EcmTexts.Localize("'{0}' is not a valid email address. To send the quick test, please enter a valid email address and try again.", current.Trim());
+        }
+      }
+      return string.Empty;
+    }
+
+    private void RunQuickTest(string recipientEmailAddresses, IEnumerable<string> variantIds, MessageItem messageItem, AbnTest abnTest)
+    {
+      if (abnTest == null || abnTest.TestCandidates.Count < 2)
+      {
+        SendMessage(recipientEmailAddresses, messageItem, null);
+      }
+      else
+      {
+        foreach (string variantId in variantIds)
+        {
+          Item item = abnTest.TestCandidates.FirstOrDefault((Item x) => x.ID == new ID(variantId));
+          if (item != null)
+          {
+            SendMessage(recipientEmailAddresses, messageItem, item);
+          }
+        }
+      }
+    }
+
+    private void SendMessage(string recipientEmailAddresses, MessageItem messageItem, Item variantTargetItem)
+    {
+      MessageItem messageItem2 = messageItem.Clone() as MessageItem;
+      if (messageItem2 != null)
+      {
+        WebPageMail webPageMail;
+        if (variantTargetItem != null && (webPageMail = (messageItem2 as WebPageMail)) != null)
+        {
+          webPageMail.TargetItem = variantTargetItem;
+        }
+        _sendingManagerFactory.GetSendingManager(messageItem2).SendTestMessage(recipientEmailAddresses, 1);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.318922/SitecoreAuthorizeAttribute.cs
@@ -0,0 +1,54 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.EmailCampaign.Server.Filters;
+using Sitecore.Security.Accounts;
+using Sitecore.Web.Authentication;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace Sitecore.Support.EmailCampaign.Server.Filters
+{
+  [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+  internal sealed class SitecoreAuthorizeAttribute : AuthorizeAttribute
+  {
+    internal interface ITicketManager
+    {
+      bool IsCurrentTicketValid();
+    }
+
+    private class TicketManagerWrapper : ITicketManager
+    {
+      public bool IsCurrentTicketValid()
+      {
+        return Sitecore.Web.Authentication.TicketManager.IsCurrentTicketValid();
+      }
+    }
+
+    private static readonly ITicketManager TicketManager = new TicketManagerWrapper();
+
+    public bool AdminsOnly
+    {
+      get;
+      set;
+    }
+
+    public SitecoreAuthorizeAttribute(params string[] roles)
+    {
+      base.Roles = string.Join(",", roles);
+    }
+
+    protected override bool IsAuthorized(HttpActionContext actionContext)
+    {
+      Assert.ArgumentNotNull(actionContext, "actionContext");
+      bool num = base.IsAuthorized(actionContext) && !AdminsOnly;
+      User user = actionContext.ControllerContext.RequestContext.Principal as User;
+      bool flag = (Account)user != (Account)null && user.IsAdministrator;
+      if (num | flag)
+      {
+        return TicketManager.IsCurrentTicketValid();
+      }
+      return false;
+    }
+  }
+}
